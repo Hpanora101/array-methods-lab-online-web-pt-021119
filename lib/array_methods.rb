8c89f6cba490f82898fb171d44f1b7487e3bab55
Henry Panora <hpanora@gmail.com>
2019-02-23 05:29:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,5 @@
 def using_include(array,element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?("Tardis")
-array.include?("wow")
+array.incude?(element)
 end
 
 def using_sort(array)
